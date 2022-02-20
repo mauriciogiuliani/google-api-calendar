87e0618b135c4d02cbceb38a63b1bb5d62a57a98
Mauricio Giuliani <mauricio_giuliani@dell.com>
2022-02-20 16:42:28 -0300

    wip

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const TOKEN_PATH = 'token.json';
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
